8056f76a980a520151a3c8b7e6e81f5732754858
ankushudapure24 <ankush.udapure@kaliba.in>
2025-04-26 19:05:15 +0530

    commit after skills. errors fix

--- portfolio-v5/src/components/skills-card.tsx
+++ portfolio-v5/src/components/skills-card.tsx
@@ -1,7 +1,7 @@
 import { RetroGrid } from "./magicui/retro-grid";
 import Image from "next/image";
 import { BorderBeam } from "./magicui/border-beam";
-import { DATA2 } from "@/data/resume";
+import { DATA } from "@/data/resume";
 
 const SkillsCard = () => {
   return (
@@ -11,7 +11,7 @@ const SkillsCard = () => {
       </div>
       <h2 className="text-xl font-bold mb-4">Skills</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-4 md:px-10">
-        {DATA2.skills.map((skill, index) => (
+        {DATA.skills.map((skill, index) => (
           <div
             key={index}
             className="bg-white/10 backdrop-blur-md border border-white/20 p-4 rounded-lg flex flex-col items-center justify-center shadow-lg hover:scale-105 transition-transform duration-300"
